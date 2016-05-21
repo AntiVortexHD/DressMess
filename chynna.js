30f4c4447efe6ae3a239ea88ee1ded448e99bb29
antivortexhd <antivortexhd@gmail.com>
2016-05-21 18:52:29 +0000

    Complete changes, still in development

---
+++ chynna.js
@@ -0,0 +1,56 @@
+     var styles = ["business", "casual", "gym", "winter", "summer", "halloween", "wedding"];
+     var clothesTypes = ["hat", "shirt", "pants"];
+
+     var data = {
+
+         winter: {
+             hat: ["images/winter/hat.png"],
+             pants: ["images/winter/pant.png"],
+             shirt: ["images/winter/shirt.png"],
+             quote: "Brrr... I'm really cold what are you going to do about it."
+         },
+
+         summer: {
+             hat: ["images/summer/hat.png"],
+             pants: ["images/summer/pant.png"],
+             shirt: ["images/summer/shirt.png"],
+             quote: "It's too hot for all these clothes what are you doing?"
+         },
+
+
+         gym: {
+             hat: ["images/gym/hat.png"],
+             pants: ["images/gym/pant.png"],
+             shirt: ["images/gym/shirt.png"],
+             quote: "I'm too lazy for all this sweating."
+         },
+
+         halloween: {
+             hat: ["images/halloween/hat.png"],
+             pants: ["images/ha/pant.png"],
+             shirt: ["images/gym/shirt.png"],
+             quote: "Ahhhh..... make me as scary as your face :p ."
+        },
+
+         wedding: {
+             hat: ["images/wedding/hat.png"],
+             pants: ["images/wedding/pant.png"],
+             shirt: ["images/wedding/shirt.png"],
+             quote: "When I said I do I didn't mean my own laundry."
+         },
+
+         business: {
+             hat: ["images/business/hat.png"],
+             pants: ["images/business/pant.png"],
+             shirt: ["images/business/shirt.png"],
+             quote: "I want to look professional not like a hipster."
+         },
+
+         casual: {
+             hat: ["images/casual/hat.png"],
+             pants: ["images/casual/pant.png"],
+             shirt: ["images/casual/shirt.png"],
+             quote: "It's impossible for you to mess up a casual style."
+         }
+     };
+     
---
+++ index.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+    var timeLeft = 60 * 1000
+    var itemRows = 4
+    var itemCols = 3
+
+
+    var currentStyle = null;
+    var points = 0;
+    var person = null;
+    var cancleInervalID = null
+
+    function isStyleCorrect() {
+        return person.shirt.style == currentStyle && person.pants.style == currentStyle && person.hat.style == currentStyle
+    }
+
+    function pickNewStyle() {
+        currentStyle = styles[Math.floor(Math.random() * styles.length)];
+        $(".text-bubble").html(data[currentStyle].quote); 
+    }
+
+
+    function resetPlayer() {
+        person = {
+            shirt: {},
+            pants: {},
+            hat: {},
+        };
+    }
+
+    function changeItems() {
+        for (var i = 0; i < itemRows; i++) {
+            for (var j = 0; j < itemCols; j++) {
+
+            }
+        }
+
+        $(".item").each(function(index, element) {
+            var style = styles[Math.floor(Math.random() * styles.length)];
+            var clothesType = clothesTypes[Math.floor(Math.random() * clothesTypes.length)];
+            var imgs = data[style][clothesType]
+            var img = imgs[Math.floor(Math.random() * imgs.length)];
+            element.setAttribute("data-type", clothesType)
+            element.setAttribute("data-style", style)
+            element.setAttribute("data-url",img)
+            
+            element.innerHTML = '<img src="' + img + '"/>'
+
+        })
+    }
+
+    function updateTimeLeft() {
+        timeLeft -= 200
+        if (timeLeft <= 0) {
+            console.log(points)
+            clearInterval(cancleInervalID)
+        }
+    }
+    resetPlayer()
+    pickNewStyle()
+    changeItems()
+    cancleInervalID = setInterval(function() {
+        if (isStyleCorrect()) {
+            points++
+            pickNewStyle()
+            resetPlayer()
+            changeItems()
+        }
+    //    updateTimeLeft()
+        console.log(points)
+    }, 200)
+    window.setplayer = function(style) {
+        person.shirt.style = style
+        person.hat.style = style
+        person.pants.style = style
+    }
+    $(".item-container .item").on("dragstart", function(event) {
+        var itemstyle = event.currentTarget.getAttribute("data-style")
+        event.originalEvent.dataTransfer.setData("style", itemstyle);
+        var itemtype = event.currentTarget.getAttribute("data-type")
+        event.originalEvent.dataTransfer.setData("type", itemtype);
+    })
+
+    $(".dropable").on("dragover", function(event) {
+        event.preventDefault()
+    })
+    $(".dropable").on("drop", function(event) {
+        event.preventDefault()
+        var itemstyle = event.originalEvent.dataTransfer.getData("style")
+        var itemtype = event.originalEvent.dataTransfer.getData("type")
+        var itemurl = event.originalEvent.dataTransfer.getData("url")
+        if (event.target.id.substring(7) == itemtype) {
+            person[itemtype].style=itemstyle
+            $("#person_"+ itemtype).css('background-image' , "url("+itemurl + ')')
+        }
+
+    })
+})
